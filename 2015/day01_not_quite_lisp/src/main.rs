9826b5095a7e6304f53678d6c4549bde367b291d
Andrei Faur <da3drus@gmail.com>
2023-01-04 20:22:19 +0100

    Add solution for 2015 day01

---
+++ 2015/day01_not_quite_lisp/src/main.rs
@@ -0,0 +1,103 @@
+use log::{debug, info};
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader, Seek},
+};
+
+fn part1<T: BufRead>(input: &mut T) -> String {
+    input
+        .lines()
+        .nth(0)
+        .unwrap()
+        .unwrap()
+        .chars()
+        .fold(0, |acc, c| {
+            debug!("{} {}", acc, c);
+            match c {
+                ')' => acc - 1,
+                '(' => acc + 1,
+                _ => unreachable!(),
+            }
+        })
+        .to_string()
+}
+
+fn part2<T: BufRead>(input: &mut T) -> String {
+    match input
+        .lines()
+        .nth(0)
+        .unwrap()
+        .unwrap()
+        .chars()
+        .zip((1..).into_iter())
+        .try_fold(0, |acc, (c, pos)| {
+            debug!("{} {}", pos, c);
+            let mut next = acc;
+            match (c, pos) {
+                (')', _) => {
+                    next -= 1;
+                }
+                ('(', _) => {
+                    next += 1;
+                }
+                _ => unreachable!(),
+            };
+            if next == -1 {
+                Err(pos)
+            } else {
+                Ok(next)
+            }
+        }) {
+        Err(pos) => pos.to_string(),
+        Ok(_) => "-1".to_string(),
+    }
+}
+
+fn main() -> io::Result<()> {
+    env_logger::init();
+
+    let f = File::open("input")?;
+    let mut reader = BufReader::new(f);
+
+    info!("Part 1 answer: {}", part1(&mut reader));
+
+    reader.rewind().unwrap();
+
+    info!("Part 2 answer: {}", part2(&mut reader));
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+    use std::io::Cursor;
+
+    fn init() {
+        let _ = env_logger::builder().is_test(true).try_init();
+    }
+
+    #[test]
+    fn part1_tests() {
+        init();
+
+        assert_eq!(part1(&mut Cursor::new("(())")), "0");
+        assert_eq!(part1(&mut Cursor::new("()()")), "0");
+        assert_eq!(part1(&mut Cursor::new("(((")), "3");
+        assert_eq!(part1(&mut Cursor::new("(()(()(")), "3");
+        assert_eq!(part1(&mut Cursor::new("))(((((")), "3");
+        assert_eq!(part1(&mut Cursor::new("())")), "-1");
+        assert_eq!(part1(&mut Cursor::new("))(")), "-1");
+        assert_eq!(part1(&mut Cursor::new(")))")), "-3");
+        assert_eq!(part1(&mut Cursor::new(")())())")), "-3");
+    }
+
+    #[test]
+    fn part2_tests() {
+        init();
+
+        assert_eq!(part2(&mut Cursor::new(")")), "1");
+        assert_eq!(part2(&mut Cursor::new("()())")), "5");
+        assert_eq!(part2(&mut Cursor::new("((((")), "-1");
+    }
+}
